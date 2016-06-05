04c862055c39bcd928f41cd341b1189eb15090ad
Sean Henry <hello@seanhenry.codes>
2016-06-05 09:22:40 +0100

    Adds project

---
+++ Sources/Dispatchable.h
@@ -0,0 +1,19 @@
+//
+//  Dispatchable.h
+//  Dispatchable
+//
+//  Created by Sean Henry on 05/06/2016.
+//  Copyright © 2016 Sean Henry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Dispatchable.
+FOUNDATION_EXPORT double DispatchableVersionNumber;
+
+//! Project version string for Dispatchable.
+FOUNDATION_EXPORT const unsigned char DispatchableVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Dispatchable/PublicHeader.h>
+
+
